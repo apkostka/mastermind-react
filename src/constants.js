498a27ce31b8bbc52a1df08fbbbe7efecaa80b5a
apkostka <apkostka@gmail.com>
2017-12-15 14:58:28 -0800

    Basic structure with player input and code generation

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+  COLORS: [
+    'red',
+    'blue',
+    'green',
+    'yellow',
+    'pink',
+    'brown',
+    'black',
+    'white'
+  ],
+
+  SETTINGS: {
+    CODE_LENGTH: [4,6,8],
+    ALLOW_DUPLICATES: [true,false]
+  },
+
+  DEFAULT_SETTINGS: {
+    CODE_LENGTH: 4,
+    ALLOW_DUPLICATES: false
+  },
+
+  NUMBER_OF_GUESSES: 10 // Make customizible?
+
+}
