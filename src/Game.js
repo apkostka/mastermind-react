e716854d006c14e9244f60f71ea333626dea710c
apkostka <apkostka@gmail.com>
2017-12-18 15:44:02 -0800

    Add stats, fix lifecycle warning

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CONSTANTS from './constants';
-import logo from './logo.svg';
-import './Game.css';
+import './App.css';
 
 function Peg(props) {
   const classes = `peg ${props.color} ${props.type}`;
@@ -18,7 +17,7 @@ function PreviousGuessPegs(props) {
 
 function PreviousGuesses(props) {
   const history = props.history.slice(0, props.history.length - 1).map((guess, i) => {
-    return <li key={i}>
+    return <li key={i} className="PreviousGuess">
       <ul><PreviousGuessPegs pegs={guess.codePegs} id={i}/></ul>
       <span>{guess.keyPegs[0]} correct, {guess.keyPegs[1]} in place</span>
     </li>;
@@ -30,7 +29,7 @@ function PreviousGuesses(props) {
 function PlayerInput(props) {
   const values = CONSTANTS.COLORS.slice(0, props.codeLength).map((color, i) => { return <option value={color} key={i}>{color}</option> });
   const inputs = props.current.codePegs.map((color, i) => {
-    return <li>
+    return <li key={i}>
       <Peg color={color} type="code" />
       <select key={i} name={i} value={color} onChange={props.handleInputChange}>{values}</select>
     </li>
@@ -58,6 +57,32 @@ class Board extends Component {
     const current = history[this.state.guessNumber];
     const guessNumber = this.state.guessNumber + 1;
 
+    return <div className="Board">
+      <PreviousGuesses
+        history={history} />
+      <PlayerInput
+        current={current}
+        codeLength={this.state.settings.CODE_LENGTH}
+        onClick={(guess) => this.submitGuess(guess)}
+        handleInputChange={(event) => this.handleInputChange(event)}/>
+      <button
+        className="renderNewBoard"
+        onClick={(i) => this.reset()}>New Board</button>
+      <p>Guess # {guessNumber}</p>
+    </div>;
+  }
+
+  handleInputChange(event) {
+    const history = this.state.history.slice();
+    history[history.length - 1].codePegs[event.target.name] = event.target.value;
+    this.setState({history: history});
+  }
+
+  submitGuess() {
+    const history = this.state.history.slice();
+    const current = history[history.length - 1];
+    current.keyPegs = checkWinner(this.state.code, current.codePegs);
+
     var endGame = false;
 
     if (current.keyPegs === true) {
@@ -70,40 +95,17 @@ class Board extends Component {
     }
 
     if (endGame) {
+      // need to learn how static work with React, this is duplicate
       this.setState({
         settings: this.props.settings,
         code: getNewCode(this.props.settings.CODE_LENGTH),
         history: [getEmptyGuess(this.props.settings.CODE_LENGTH)],
         guessNumber: 0
       });
+      this.props.incrementWins(current.keyPegs === true);
+      return;
     }
 
-    return <div>
-      <PreviousGuesses
-        history={history} />
-      <PlayerInput
-        current={current}
-        codeLength={this.state.settings.CODE_LENGTH}
-        onClick={(guess) => this.submitGuess(guess)}
-        handleInputChange={(event) => this.handleInputChange(event)}/>
-      <button
-        className="renderNewBoard"
-        onClick={i => this.reset()}>New Board</button>
-      <p>Guess # {guessNumber}</p>
-    </div>;
-  }
-
-  handleInputChange(event) {
-    const history = this.state.history.slice();
-    history[history.length - 1].codePegs[event.target.name] = event.target.value;
-    this.setState({history: history});
-  }
-
-  submitGuess() {
-    console.log(this);
-    const history = this.state.history.slice();
-    const current = history[history.length - 1];
-    current.keyPegs = checkWinner(this.state.code, current.codePegs);
     this.setState({
       history: history.concat(JSON.parse(JSON.stringify(current))), // deep clone, fix?
       guessNumber: this.state.guessNumber + 1
@@ -111,24 +113,52 @@ class Board extends Component {
   }
 
   reset() {
-    this.setState({guessNumber: CONSTANTS.NUMBER_OF_GUESSES + 1});
+    this.setState({
+      settings: this.props.settings,
+      code: getNewCode(this.props.settings.CODE_LENGTH),
+      history: [getEmptyGuess(this.props.settings.CODE_LENGTH)],
+      guessNumber: 0
+    });
   }
 }
 
-class Game extends Component {
+function Settings(props) {
+ return <div></div>;
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      settings: Object.assign({}, CONSTANTS.DEFAULT_SETTINGS),
+      wins: 0,
+      games: 0
+    }
+  }
+
+  incrementWins(isWin) {
+    this.setState({
+      wins: isWin ? this.state.wins + 1 : this.state.wins,
+      games: this.state.games + 1
+    });
+  }
+
   render() {
-    const settings = Object.assign({}, CONSTANTS.DEFAULT_SETTINGS);
+    const winAvg = this.state.games === 0 ? 0 : Math.round( (this.state.wins / this.state.games) * 100 );
 
     return (
       <div className="App">
-        <Board settings={settings}>
-        </Board>
+        <Board
+          settings={this.state.settings}
+          incrementWins={i => this.incrementWins(i)} />
+        <Settings setting={this.state.settings} />
+        <div className="stats">{this.state.wins} Wins, {this.state.losses} Losses, {winAvg}%</div>
       </div>
     );
   }
 }
 
-export default Game;
+export default App;
 
 function getNewCode(codeLength) {
   return [...new Array(codeLength)]
@@ -147,7 +177,7 @@ function checkWinner(code, guess) {
   var numCorrect = 0;
   var numInPlace = 0;
 
-  for (var x in guess) {
+  for (let x in guess) {
     var match = check.indexOf(guess[x]);
     if (match >= 0) {
       check.splice(match, 1);
@@ -155,7 +185,7 @@ function checkWinner(code, guess) {
     }
   }
 
-  for (var x in guess) {
+  for (let x in guess) {
     if (guess[x] === code[x]) numInPlace++;
   }
 
